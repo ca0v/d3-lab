f95d43fb0915b85bcabaea7dd32894f6eafcc762
Corey Alix <coreyalix@gmail.com>
2023-01-10 15:18:24 -0500

    Adds grid view

--- src/main.ts
+++ src/main.ts
@@ -21,71 +21,91 @@ const data = [
   date: nameOfMonth(new Date(v.date)),
 }))
 
-// create a bar chart
-const svg = d3.select("svg")
-const margin = { top: 20, right: 20, bottom: 60, left: 60 }
-const width = +svg.attr("width") - margin.left - margin.right
-const height = +svg.attr("height") - margin.top - margin.bottom
-const g = svg
-  .append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+function renderBarChart() {
+  // create a bar chart
+  const svg = d3.select("#app > svg")
+  const margin = { top: 20, right: 20, bottom: 60, left: 60 }
+  const width = +svg.attr("width") - margin.left - margin.right
+  const height = +svg.attr("height") - margin.top - margin.bottom
+  const g = svg
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 
-const xScale = d3
-  .scaleBand()
-  .rangeRound([0, width])
-  .padding(0.3)
-  .domain(data.map((d) => d.date))
+  const xScale = d3
+    .scaleBand()
+    .rangeRound([0, width])
+    .padding(0.3)
+    .domain(data.map((d) => d.date))
 
-const maxPrice = d3.max(data, (d) => d.price)!
-// maxPrice to rounded up to nearest 100
-const maxPriceRounded = Math.ceil(maxPrice / 100) * 100
+  const maxPrice = d3.max(data, (d) => d.price)!
+  // maxPrice to rounded up to nearest 100
+  const maxPriceRounded = Math.ceil(maxPrice / 100) * 100
 
-const yScale = d3
-  .scaleLinear()
-  .rangeRound([height, 0])
-  .domain([0, maxPriceRounded])
+  const yScale = d3
+    .scaleLinear()
+    .rangeRound([height, 0])
+    .domain([0, maxPriceRounded])
 
-const makeYLines = () =>
-  d3
-    .axisRight(yScale)
-    .ticks(5)
-    .tickSize(width)
-    .tickFormat(() => "")
+  const makeYLines = () =>
+    d3
+      .axisRight(yScale)
+      .ticks(5)
+      .tickSize(width)
+      .tickFormat(() => "")
 
-// draw horizontal lines
-g.append("g").attr("class", "grid").call(makeYLines())
+  // draw horizontal lines
+  g.append("g").attr("class", "grid").call(makeYLines())
 
-g.selectAll(".bar")
-  .data(data)
-  .enter()
-  .append("rect")
-  .attr("class", "bar")
-  .attr("x", (d) => xScale(d.date)!)
-  .attr("y", (d) => yScale(d.price))
-  .attr("width", xScale.bandwidth())
-  .attr("height", (d) => height - yScale(d.price))
+  g.selectAll(".bar")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("class", "bar")
+    .attr("x", (d) => xScale(d.date)!)
+    .attr("y", (d) => yScale(d.price))
+    .attr("width", xScale.bandwidth())
+    .attr("height", (d) => height - yScale(d.price))
 
-g.selectAll(".bar-label")
-  .data(data)
-  .enter()
-  .append("text")
-  .attr("class", "bar-label")
-  .attr("x", (d) => xScale(d.date)!)
-  .attr("y", (d) => yScale(d.price) - 10)
-  .text((d) => d.price.toFixed(2))
+    // text above bars
+  g.selectAll(".bar-label")
+    .data(data)
+    .enter()
+    .append("text")
+    .attr("class", "bar-label")
+    .attr("x", (d) => xScale(d.date)!)
+    .attr("y", (d) => yScale(d.price) - 10)
+    .text((d) => asUsd(d.price))
 
-// x-axis, bottom
-g.append("g")
-  .attr("class", "axis axis--x")
-  .attr("transform", `translate(0,${height})`)
-  .call(d3.axisBottom(xScale))
+  // x-axis, bottom
+  g.append("g")
+    .attr("class", "axis axis--x")
+    .attr("transform", `translate(0,${height})`)
+    .call(d3.axisBottom(xScale))
 
-// y-axis with title
-g.append("g")
-  .attr("class", "axis axis--y")
-  .call(d3.axisLeft(yScale).ticks(5, ".0f"))
+  // y-axis with title
+  g.append("g")
+    .attr("class", "axis axis--y")
+    .call(d3.axisLeft(yScale).ticks(5, ".0f"))
+}
 
-function nameOfMonth(date: Date): any {
+function renderGrid() {
+  const gridContainer = d3.select("#app > .grid")
+  if (!gridContainer) throw new Error("Grid not found")
+
+  const rows = gridContainer
+    .selectAll(".row")
+    .data(data)
+    .enter()
+    .append("div")
+    .attr("class", "row")
+
+  // append the date and price to the row
+  rows.append("div").attr("class", "cell date").text(d => d.date)
+
+  rows.append("div").attr("class", "cell currency").text(d => asUsd(d.price))
+}
+
+function nameOfMonth(date: Date) {
   const monthNames = [
     "January",
     "February",
@@ -105,4 +125,12 @@ function nameOfMonth(date: Date): any {
   const month = date.getUTCMonth()
   return monthNames[month]
 }
-// Path: index.html
+
+function asUsd(value: number) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(value)
+
+}
+export { renderBarChart, renderGrid }
